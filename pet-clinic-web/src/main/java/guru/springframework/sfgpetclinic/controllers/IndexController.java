4e4352c50d49c10985534d3dc235211a29a45aa6
Mihail Calaras <mihail.calaras@reigncode.dev>
2020-11-20 16:04:34 +0200

    added index page and controller. closes #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+  @RequestMapping({"", "/", "index", "index.html"})  // When there's a request that comes in to the
+                                                     // empty string, slash, index,
+                                                     // or index.html, then they're all going to
+                                                     // to match to this RequestMapping.
+  public String index() {
+
+    return "index";
+
+  }
+
+}
