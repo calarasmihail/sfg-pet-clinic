aac0a0690ca5abab2c1f907ec42fe31964a4f3c8
Mihail Calaras <mihail.calaras@reigncode.dev>
2021-01-10 14:10:54 +0200

    refactored map implimentation to abstract ID generation. closes #23

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,16 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-  protected Map<ID, T> map = new HashMap<>();
+  protected Map<Long, T> map = new HashMap<>();
 
   Set<T> findAll() {                         // findAll() is just going to return back a new
                                              // HashSet of the values.
@@ -21,7 +24,7 @@ public abstract class AbstractMapService<T, ID> {
     return map.get(id);     // We return here back that object out of the map.
   }
 
-  T save(ID id, T object) {        // I'm just saying object there because we really don't
+  T save(T object) {        // I'm just saying object there because we really don't
                                    // know what's coming in because of the generics. Now we can
                                    // say map.put():
 
@@ -29,7 +32,14 @@ public abstract class AbstractMapService<T, ID> {
                                    // have to
                                    // override this a little bit. What we ca do is
                                    // T save(ID id, T object) instead of initial T save(T object)
-    map.put(id, object);
+    if (object != null) {
+      if (object.getId() == null) {
+        object.setId(getNextId());
+      }
+      map.put(object.getId(), object);
+    } else {
+      throw new RuntimeException("Object cannot be null");
+    }
     return object;
   }
 
@@ -44,6 +54,19 @@ public abstract class AbstractMapService<T, ID> {
   void delete(T object) {
     map.entrySet().removeIf(entry -> entry.getValue().equals(object)); // Get that all in one
                                                                        // statement.
+  }
+
+  private Long getNextId() {
+
+    Long nextId = null;
+
+    try {
+      nextId =  Collections.max(map.keySet()) + 1;  // get max of that
+    } catch (NoSuchElementException e) {
+      nextId = 1L;
+    }
+
+    return nextId;
   }
 
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -23,13 +23,11 @@ public class DataLoader implements CommandLineRunner {
   public void run(String... args) throws Exception {
 
     Owner owner1 = new Owner();
-    owner1.setId(1L);
     owner1.setFirstName("Josh");
     owner1.setLastName("Remember");
     ownerService.save(owner1);
 
     Owner owner2 = new Owner();
-    owner2.setId(2L);
     owner2.setFirstName("Michael");
     owner2.setLastName("Frost");
     ownerService.save(owner2);
@@ -37,13 +35,11 @@ public class DataLoader implements CommandLineRunner {
     System.out.println("Loaded Owners...");
 
     Vet vet1 = new Vet();
-    vet1.setId(1L);
     vet1.setFirstName("Ion");
     vet1.setLastName("Voltaire");
     vetService.save(vet1);
 
     Vet vet2 = new Vet();
-    vet2.setId(2L);
     vet2.setFirstName("Jack");
     vet2.setLastName("Dream");
     vetService.save(vet2);
